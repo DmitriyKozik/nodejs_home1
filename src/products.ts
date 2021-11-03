c8261c10f582d4a506347b9d9782c8671ceac34d
DmitriyKozik <dmitriiy.kozik@gmail.com>
2021-11-03 13:41:46 +0200

    edit1

--- src/products.ts
+++ src/products.ts
@@ -1,13 +1,9 @@
-export function Products(product_number: number) {
-    const product_key = product_number-1;
-
+export function Products() {
     const fs = require('fs');
 
     const filePath = "src/products.json";
     const products_content = fs.readFileSync(filePath, "utf8");
     const products = JSON.parse(products_content);
 
-    let result = products[product_key];
-
-    return result;
+    return products;
 }

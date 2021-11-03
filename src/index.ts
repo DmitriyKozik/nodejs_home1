7236c320978c48ee26a13d1bb64902a185a9622f
DmitriyKozik <dmitriiy.kozik@gmail.com>
2021-11-03 13:44:48 +0200

    edit2

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@ const products = Products();
 console.timeEnd('products_init');
 
 console.time('product_50');
-console.log(products[(50 - 1)])
+console.log(products[(50 - 1)]);
 console.timeEnd('product_50');
